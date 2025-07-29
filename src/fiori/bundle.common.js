83ca263315a18d5449b1b8f462ce9dd99e07de89
Johannes Doberer <johannes.doberer@sap.com>
2025-07-29 13:58:48 +0200

    update ui5 webcomponent bundle (#2)

--- src/fiori/bundle.common.js
+++ src/fiori/bundle.common.js
@@ -1,32 +1,55 @@
 // FIORI features
-import "@ui5/webcomponents-fiori/dist/features/CoPilotAnimation.js";
+// import "@ui5/webcomponents-fiori/dist/features/CoPilotAnimation.js";
 
 // FIORI illustrations
 import "@ui5/webcomponents-fiori/dist/illustrations/AllIllustrations.js";
 
 // FIORI components
-import Bar from "@ui5/webcomponents-fiori/dist/Bar.js";
+import BarcodeScannerDialog from "@ui5/webcomponents-fiori/dist/BarcodeScannerDialog.js";
+import DynamicPage from "@ui5/webcomponents-fiori/dist/DynamicPage.js";
+import DynamicPageHeader from "@ui5/webcomponents-fiori/dist/DynamicPageHeader.js";
+import DynamicPageTitle from "@ui5/webcomponents-fiori/dist/DynamicPageTitle.js";
 import DynamicSideContent from "@ui5/webcomponents-fiori/dist/DynamicSideContent.js";
-import FilterItem from "@ui5/webcomponents-fiori/dist/FilterItem.js";
-import FilterItemOption from "@ui5/webcomponents-fiori/dist/FilterItemOption.js";
 import FlexibleColumnLayout from "@ui5/webcomponents-fiori/dist/FlexibleColumnLayout.js";
 import IllustratedMessage from "@ui5/webcomponents-fiori/dist/IllustratedMessage.js";
 import MediaGallery from "@ui5/webcomponents-fiori/dist/MediaGallery.js";
 import MediaGalleryItem from "@ui5/webcomponents-fiori/dist/MediaGalleryItem.js";
+import NavigationLayout from "@ui5/webcomponents-fiori/dist/NavigationLayout.js";
+import NotificationList from "@ui5/webcomponents-fiori/dist/NotificationList.js";
+import NotificationListGroupItem from "@ui5/webcomponents-fiori/dist/NotificationListGroupItem.js";
+import NotificationListItem from "@ui5/webcomponents-fiori/dist/NotificationListItem.js";
+import Page from "@ui5/webcomponents-fiori/dist/Page.js";
 import ProductSwitch from "@ui5/webcomponents-fiori/dist/ProductSwitch.js";
 import ProductSwitchItem from "@ui5/webcomponents-fiori/dist/ProductSwitchItem.js";
+import Search from "@ui5/webcomponents-fiori/dist/Search.js";
+import SearchItem from "@ui5/webcomponents-fiori/dist/SearchItem.js";
+import SearchItemGroup from "@ui5/webcomponents-fiori/dist/SearchItemGroup.js";
+import SearchMessageArea from "@ui5/webcomponents-fiori/dist/SearchMessageArea.js";
+import SearchScope from "@ui5/webcomponents-fiori/dist/SearchScope.js";
+import ShellBar from "@ui5/webcomponents-fiori/dist/ShellBar.js";
+import ShellBarBranding from "@ui5/webcomponents-fiori/dist/ShellBarBranding.js";
+import ShellBarItem from "@ui5/webcomponents-fiori/dist/ShellBarItem.js";
+import ShellBarSearch from "@ui5/webcomponents-fiori/dist/ShellBarSearch.js";
+import ShellBarSpacer from "@ui5/webcomponents-fiori/dist/ShellBarSpacer.js";
 import SideNavigation from "@ui5/webcomponents-fiori/dist/SideNavigation.js";
+import SideNavigationGroup from "@ui5/webcomponents-fiori/dist/SideNavigationGroup.js";
 import SideNavigationItem from "@ui5/webcomponents-fiori/dist/SideNavigationItem.js";
 import SideNavigationSubItem from "@ui5/webcomponents-fiori/dist/SideNavigationSubItem.js";
-import Page from "@ui5/webcomponents-fiori/dist/Page.js";
-import ShellBar from "@ui5/webcomponents-fiori/dist/ShellBar.js";
-import ShellBarItem from "@ui5/webcomponents-fiori/dist/ShellBarItem.js";
-import SortItem from "@ui5/webcomponents-fiori/dist/SortItem.js";
 import Timeline from "@ui5/webcomponents-fiori/dist/Timeline.js";
+import TimelineGroupItem from "@ui5/webcomponents-fiori/dist/TimelineGroupItem.js";
+import TimelineItem from "@ui5/webcomponents-fiori/dist/TimelineItem.js";
 import UploadCollection from "@ui5/webcomponents-fiori/dist/UploadCollection.js";
 import UploadCollectionItem from "@ui5/webcomponents-fiori/dist/UploadCollectionItem.js";
-import NotificationListItem from "@ui5/webcomponents-fiori/dist/NotificationListItem.js"
-import NotificationListGroupItem from "@ui5/webcomponents-fiori/dist/NotificationListGroupItem.js";
-import NotificationAction from "@ui5/webcomponents-fiori/dist/NotificationAction.js";
-import Wizard from "@ui5/webcomponents-fiori/dist/Wizard.js";
+import UserMenu from "@ui5/webcomponents-fiori/dist/UserMenu.js";
+import UserMenuAccount from "@ui5/webcomponents-fiori/dist/UserMenuAccount.js";
+import UserMenuItem from "@ui5/webcomponents-fiori/dist/UserMenuItem.js";
+import UserMenuItemGroup from "@ui5/webcomponents-fiori/dist/UserMenuItemGroup.js";
+import UserSettingsDialog from "@ui5/webcomponents-fiori/dist/UserSettingsDialog.js";
+import UserSettingsItem from "@ui5/webcomponents-fiori/dist/UserSettingsItem.js";
+import UserSettingsView from "@ui5/webcomponents-fiori/dist/UserSettingsView.js";
 import ViewSettingsDialog from "@ui5/webcomponents-fiori/dist/ViewSettingsDialog.js";
+import FilterItem from "@ui5/webcomponents-fiori/dist/FilterItem.js";
+import FilterItemOption from "@ui5/webcomponents-fiori/dist/FilterItemOption.js";
+import SortItem from "@ui5/webcomponents-fiori/dist/SortItem.js";
+import Wizard from "@ui5/webcomponents-fiori/dist/Wizard.js";
+import WizardStep from "@ui5/webcomponents-fiori/dist/WizardStep.js";
